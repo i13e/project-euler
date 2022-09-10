5474102e3c6e29e3b5f17c17cd9cbf87ae56817b
Ian Breckenridge <62034540+i13e@users.noreply.github.com>
2022-09-10 18:02:46 -0400

    Huuuge improvement to euler #3

--- python/003-largest-prime-factor.py
+++ python/003-largest-prime-factor.py
@@ -1,9 +1,18 @@
-target = 600851475143
-left = 0
-right = target - 1
+def primeFactor(target):
+    # Divide target by 2 until it is odd
+    while target % 2 == 0:
+        target /= 2
+
+    # target is now odd, so we can skip even numbers
+    for num in range(3, int(target ** (0.5)) + 1, 2):
+        while target % num == 0:
+            target /= num
+        # Print only the largest prime factor
+        if target == 1:
+            return num
+    # If loop ends, target is prime
+    if target > 2:
+        return target
 
 
-while left <= right:
-    mid = (left + right) // 2
-    if target % mid == 0:
-        break
+print(primeFactor(600851475143))
