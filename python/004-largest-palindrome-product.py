69ce9d0f67460d4cbc5fe983ff2eb301928d99b5
Ian Breckenridge <62034540+i13e@users.noreply.github.com>
2022-09-10 18:58:40 -0400

    add numerical solution for #4

--- python/004-largest-palindrome-product.py
+++ python/004-largest-palindrome-product.py
@@ -1,14 +1,25 @@
 def palindromeProduct(min, max):
-    result = 0
+    res = 0
     for x in range(max, min, -1):
         for y in range(max, min, -1):
-            product = x * y
-            if str(product) == str(product)[::-1] and product > result:
-                result = product
+            prod = tmp = x * y
+            # Numerical solution
+            if tmp % 10 == 0:
+                continue
+            rev = 0
+            while tmp > rev:
+                rev = rev * 10 + tmp % 10
+                tmp = tmp // 10
+            if (tmp == rev or tmp == rev // 10) and prod > res:
+                res = prod
                 break
-            elif result > product:
+            # String solution
+            # if str(prod) == str(prod)[::-1] and prod > res:
+            #     res = prod
+            #     break
+            elif prod < res:
                 break
-    return result
+    return res
 
 
 print(palindromeProduct(100, 1000))
