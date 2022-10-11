055b4b42bddb331c1e9da837ff429086277d1eeb
Ian Breckenridge <62034540+i13e@users.noreply.github.com>
2022-10-11 15:50:32 -0400

    use sieve of Eratosthenes to calculate primes

--- python/004-largest-palindrome-product.py
+++ python/004-largest-palindrome-product.py
@@ -1,21 +1,22 @@
-def palindromeProduct(min: int, max: int, res=0):
-    for x in range(max, min, -1):
-        for y in range(max, min, -1):
-            # Numerical solution
-            prod, rev = x * y, 0
-            while prod > 0:
-                rev = rev * 10 + prod % 10
-                prod //= 10
-            if (x * y == rev or x * y == rev // 10) and x * y > res:
-                res = x * y
-                break
-            # String solution
-            # if str(x * y) == str(x * y)[::-1] and x * y > res:
-            #     res = prod
-            #     break
-            elif x * y < res:
-                break
-    return res
+class Solution:
+    def palindromeProduct(self, min: int, max: int, res=0):
+        for x in range(max, min, -1):
+            for y in range(max, min, -1):
+                # Numerical solution
+                prod, rev = x * y, 0
+                while prod > 0:
+                    rev = rev * 10 + prod % 10
+                    prod //= 10
+                if (x * y == rev or x * y == rev // 10) and x * y > res:
+                    res = x * y
+                    break
+                # String solution
+                # if str(x * y) == str(x * y)[::-1] and x * y > res:
+                #     res = prod
+                #     break
+                elif x * y < res:
+                    break
+        return res
 
 
-print(palindromeProduct(100, 1000))
+print(Solution().palindromeProduct(100, 1000))
--- python/005-smallest-multiple.py
+++ python/005-smallest-multiple.py
@@ -1,15 +1,15 @@
 def smallestMultiple(target: int, res=1):
-    for num in range(2, target + 1):
-        # Remove composite numbers
-        if res % num == 0:
-            continue
-        # Reduce perfect powers into primes
-        for prime in range(2, int(num**0.5) + 1):
-            if num % prime == 0:
-                num = prime
-                break
-        # Multiply result by the remaining prime
-        res *= num
+    if not target:
+        return 0
+    primes = [1] * (target + 1)
+    primes[0] = primes[1] = 0
+    for n in range(2, target + 1):
+        if primes[n]:
+            primes[n**2 : target + 1 : n] = [0] * (1 + (target - n**2) // n)
+            power = 2
+            while n ** power in range(target + 1):
+                power += 1
+            res *= n ** (power - 1)
     return res
 
 
