5d659a5a0bf64422e5c83e229f9732b339b59c10
Ian Breckenridge <62034540+i13e@users.noreply.github.com>
2023-10-06 15:55:33 -0400

    fix: Typing and default values

--- python/0013.py
+++ python/0013.py
@@ -1,7 +1,7 @@
-def main():
+def main() -> int:
     with open("resources/0013_numbers.txt") as f:
         numbers = [int(line) for line in f]
-    return str(sum(numbers))[:10]
+    return int(str(sum(numbers))[:10])
 
 
 if __name__ == "__main__":
--- python/0020.py
+++ python/0020.py
@@ -1,8 +1,8 @@
 from math import factorial
 
-def main(n: int) -> int:
+def main(n: int = 100) -> int:
     return sum(map(int, str(factorial(n))))
 
 
 if __name__ == "__main__":
-    print(main(100))
+    print(main())
