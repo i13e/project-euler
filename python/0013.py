7bf9cd05657d0d174ee9f38ba527e845d87a7f57
Ian Breckenridge <62034540+i13e@users.noreply.github.com>
2023-10-10 15:11:55 -0400

    fix: return as str

--- python/0013.py
+++ python/0013.py
@@ -1,7 +1,7 @@
-def main() -> int:
+def main() -> str:
     with open("resources/0013_numbers.txt") as f:
         numbers = [int(line) for line in f]
-    return int(str(sum(numbers))[:10])
+    return str(sum(numbers))[:10]
 
 
 if __name__ == "__main__":
